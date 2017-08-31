92a93fc44d7cbf95d166ae6fa1972b99934c26e8
Daniel Arroyo <daniel.arroyo@gmail.com>
2017-08-31 22:27:51 +0300

    first commit

---
+++ api.demo.js
@@ -0,0 +1,13 @@
+function import_url()
+{
+  astroprint.importDesign("https://www.thingiverse.com/download:2101202", "test.stl");
+}
+
+function import_blob()
+{
+  var fileInput = document.getElementById('the-file');
+  var file = fileInput.files[0];
+  if (file) {
+    astroprint.importDesignByBlob(file, file.type, file.name);
+  }
+}
